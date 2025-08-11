9eeb3e76626a4f6c3f08ed35d6ac3f1da06642b5
RenanSilvaMartins <renansilva.m09@gmail.com>
2025-08-11 08:29:45 -0300

    nova página de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_itech/app_widget.dart';
 
 void main() {
-  runApp(AppWidget(title: 'Renanrando 3'));
+  runApp(MaterialApp(home: HomePage()));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [?Colors.purple[800], ?Colors.purple[600]],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Login', style: TextStyle(color: Colors.white, fontSize: 40)),
+                  SizedBox(height: 10),
+                  Text('Bem vindo Novamente', style: TextStyle(color: Colors.white, fontSize: 18)),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container( 
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
