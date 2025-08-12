77701c37306e973408ef3593610065a9d818c784
RenanSilvaMartins <renansilva.m09@gmail.com>
2025-08-12 08:30:20 -0300

    atualização-MOBILE-tentar conectar com back e banco

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+  runApp(
+    MaterialApp(
+      // Recomendo remover o banner de debug para visualizar melhor
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -9,11 +15,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 30),
+        // A LINHA DE PADDING FOI REMOVIDA DAQUI
         width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [?Colors.purple[800], ?Colors.purple[600]],
+            begin: Alignment.topCenter, // Um bom começo para o gradiente
+            colors: [
+              Colors.purple[800]!, // Adicione '!' para non-null
+              Colors.purple[600]!, // Adicione '!' para non-null
+            ],
           ),
         ),
         child: Column(
@@ -25,15 +35,21 @@ class HomePage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text('Login', style: TextStyle(color: Colors.white, fontSize: 40)),
+                Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
                   SizedBox(height: 10),
-                  Text('Bem vindo Novamente', style: TextStyle(color: Colors.white, fontSize: 18)),
+                  Text(
+                    'Bem vindo Novamente',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
                 ],
               ),
             ),
             SizedBox(height: 20),
             Expanded(
-              child: Container( 
+              child: Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -41,8 +57,103 @@ class HomePage extends StatelessWidget {
                     topRight: Radius.circular(60),
                   ),
                 ),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 60,),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFFA626A6),
+                              blurRadius: 20,
+                              offset: Offset(0, 10),
+                            ),
+                          ],
+                        ),
+                        child: Column(children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(color: Colors.grey))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Digite aqui seu email",
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none
+                              ),
+                            ),
+                            ),
+                            Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(color: Colors.grey))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Senha",
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none
+                              ),
+                            ),
+                            ),
+                        ]),
+                      ),
+                      SizedBox(height: 40,),
+                      Text("Esqueci a Senha?", style: TextStyle(color: Colors.grey),),
+                      SizedBox(height: 40,),
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.purple
+                        ),
+                        child: Center(
+                          child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                      SizedBox(height: 50,),
+                      Text("Continua com midias sociais", style: TextStyle(color: Colors.grey),),
+                      SizedBox(height: 30,),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.blue
+                              ),
+                              child: Center(
+                                child: Text("Facebook", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 30,),
+                          Expanded(
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.black
+                              ),
+                              child: Center(
+                                child: Text("Github", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  // DICA: Adicione o seu conteúdo de formulário aqui dentro
+                  // child: SingleChildScrollView( ... )
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
